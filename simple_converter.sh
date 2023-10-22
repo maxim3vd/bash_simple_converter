9cd843a9a37f2e5eda4e94b0b6b0754a1b15f30e
diplocodus <maxim3.vd@gmail.com>
2023-10-22 10:56:22 +0200

    refactoring by adding functions

--- simple_converter.sh
+++ simple_converter.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/bash
 
-echo "Enter a definition:"
-read -a user_input
-arr_length="${#user_input[@]}"
-definition="${user_input[0]}"
-constant="${user_input[1]}"
-re_def='[A-Za-z]_to_[A-Za-z]'
-re_const='^[+-]?[0-9]+\.?[0-9]*$'
-if [ $arr_length -eq 2 ] && [[ $definition =~ $re_def ]] && [[ $constant =~ $re_const ]];then
-    echo "The definition is correct!"
-else
-    echo "The definition is incorrect!"
-fi
+simple_converter() {
+    echo "Enter a definition:"
+    read -a user_input
+    arr_length="${#user_input[@]}"
+    definition="${user_input[0]}"
+    constant="${user_input[1]}"
+    check_definition
+    isValidDefinition=$?
+}
+
+check_definition() {
+    re_def='[A-Za-z]_to_[A-Za-z]'
+    re_const='^[+-]?[0-9]+\.?[0-9]*$'
+    if [ $arr_length -eq 2 ] && [[ $definition =~ $re_def ]] && [[ $constant =~ $re_const ]];then
+        echo "The definition is correct!"
+        return 1
+    else
+        echo "The definition is incorrect!"
+        return 0
+    fi
+}
+
+simple_converter
