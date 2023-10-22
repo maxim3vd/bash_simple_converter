f354400deeee9f82da4bcedd7202f6bda4146fb6
diplocodus <maxim3.vd@gmail.com>
2023-10-22 15:50:41 +0200

    refactoring code

--- simple_converter.sh
+++ simple_converter.sh
@@ -6,30 +6,29 @@ simple_converter() {
     arr_length="${#user_input[@]}"
     definition="${user_input[0]}"
     constant="${user_input[1]}"
-    check_definition
+    check_definition $arr_length $definition $constant
     isValidDefinition=$?
     if [ $isValidDefinition -eq 1 ]; then
         echo "Enter a value to convert:"
         read user_value
-        check_value
-        conversion
+        check_value $user_value
+        conversion $constant $user_value
     fi
 }
 
 check_definition() {
     re_def='[A-Za-z]_to_[A-Za-z]'
     re_const='^[+-]?[0-9]+\.?[0-9]*$'
-    if [ $arr_length -eq 2 ] && [[ $definition =~ $re_def ]] && [[ $constant =~ $re_const ]];then
+    if [ $1 -eq 2 ] && [[ $2 =~ $re_def ]] && [[ $3 =~ $re_const ]]; then
         return 1
     else
         echo "The definition is incorrect!"
-        return 0
     fi
 }
 
 check_value() {
-    re_value='^[0-9]+([.][0-9]+)?$'
-    while [[ ! $user_value =~ $re_value ]];
+    re_val='^[0-9]+([.][0-9]+)?$'
+    while [[ ! $1 =~ $re_val ]];
     do
         echo "Enter a float or integer value!"
         read user_value
@@ -37,7 +36,7 @@ check_value() {
 }
 
 conversion() {
-    result=$(echo "$constant * $user_value" | bc)
+    result=$(echo "$1 * $2" | bc)
     echo "Result: $result"
 }
 
