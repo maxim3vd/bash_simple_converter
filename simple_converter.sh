47884a3f0074413dd8b7458cbe02800e167cf857
diplocodus <maxim3.vd@gmail.com>
2023-10-22 09:07:43 +0200

    add logic to check wether the definition is correct or not

--- simple_converter.sh
+++ simple_converter.sh
@@ -7,3 +7,8 @@ definition="${user_input[0]}"
 constant="${user_input[1]}"
 re_def='[A-Za-z]_to_[A-Za-z]'
 re_const='^[+-]?[0-9]+\.?[0-9]*$'
+if [ $arr_length -eq 2 ] && [[ $definition =~ $re_def ]] && [[ $constant =~ $re_const ]];then
+    echo "The definition is correct!"
+else
+    echo "The definition is incorrect!"
+fi
